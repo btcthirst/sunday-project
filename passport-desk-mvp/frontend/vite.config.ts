7dbd6aaf2a6de65d9d2b42cd95b4921fa893e161
btcthirst <btcthirst@gmail.com>
2026-02-01 09:27:26 +0200

    fixed issuses stage 1

--- passport-desk-mvp/frontend/vite.config.ts
+++ passport-desk-mvp/frontend/vite.config.ts
@@ -1,7 +1,34 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        {
+          'naive-ui': [
+            'useDialog',
+            'useMessage',
+            'useNotification',
+            'useLoadingBar'
+          ]
+        }
+      ],
+      dts: 'src/auto-imports.d.ts'
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()],
+      dts: 'src/components.d.ts'
+    })
+  ],
+  define: {
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
+  }
 })
